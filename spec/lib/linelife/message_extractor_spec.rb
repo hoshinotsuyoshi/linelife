7d5a40b58d5e8b11373522491c39ed956fce0e9c
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-04-13 23:23:12 +0900

    Modify spec

--- spec/lib/linelife/message_extractor_spec.rb
+++ spec/lib/linelife/message_extractor_spec.rb
@@ -1,49 +1,51 @@
 describe Linelife::MessageExtractor do
-  describe '#content_type' do
-    include_context 'inbound message is contentType:1'
-
-    it do
-      messages = Linelife::MessageExtractor.new.do_extract(json)
-      expect(messages.first.content_type).to be :text
-    end
-  end
-
-  describe '#from' do
-    include_context 'inbound message is contentType:1'
-
-    it do
-      messages = Linelife::MessageExtractor.new.do_extract(json)
-      expect(messages.first.from).to eq 'ueddddddddddddddddddddddddddddddd'
-    end
-  end
-
-  describe '#operation? and #message?' do
-    context 'given normal message' do
+  describe '#do_extract' do
+    describe 'returns object which responds to :content_type' do
       include_context 'inbound message is contentType:1'
 
       it do
         messages = Linelife::MessageExtractor.new.do_extract(json)
-        expect(messages.first).to be_message
-        expect(messages.first).not_to be_operation
+        expect(messages.first.content_type).to be :text
       end
     end
 
-    context 'given operation' do
-      include_context 'inbound message is opType:4'
+    describe 'returns object which responds to :from' do
+      include_context 'inbound message is contentType:1'
+
       it do
         messages = Linelife::MessageExtractor.new.do_extract(json)
-        expect(messages.first).not_to be_message
-        expect(messages.first).to be_operation
+        expect(messages.first.from).to eq 'ueddddddddddddddddddddddddddddddd'
       end
     end
-  end
 
-  describe '#added_as_friend?' do
-    include_context 'inbound message is opType:4'
+    describe 'returns object which responds to :message? and :operation?' do
+      context 'given normal message' do
+        include_context 'inbound message is contentType:1'
 
-    it do
-      messages = Linelife::MessageExtractor.new.do_extract(json)
-      expect(messages.first).to be_added_as_friend
+        it do
+          messages = Linelife::MessageExtractor.new.do_extract(json)
+          expect(messages.first).to be_message
+          expect(messages.first).not_to be_operation
+        end
+      end
+
+      context 'given operation' do
+        include_context 'inbound message is opType:4'
+        it do
+          messages = Linelife::MessageExtractor.new.do_extract(json)
+          expect(messages.first).not_to be_message
+          expect(messages.first).to be_operation
+        end
+      end
+    end
+
+    describe 'returns object which responds to :added_as_friend?' do
+      include_context 'inbound message is opType:4'
+
+      it do
+        messages = Linelife::MessageExtractor.new.do_extract(json)
+        expect(messages.first).to be_added_as_friend
+      end
     end
   end
 end
